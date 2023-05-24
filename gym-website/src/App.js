8868b2ad11b8afe8f75761f16bf7f36a8333de55
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-05-24 19:34:17 -0300

    at

---
+++ gym-website/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Hero from './components/Hero/Hero';
+import Programs from './components/programs/Programs.jsx';
+import Reasons from './components/Reasons/Reasons.jsx'
+import Plans from './components/Plans/Plans.jsx'
+import Testimonials from './assets/Testimonials/Testimonials';
+import Join from './components/Join/Join.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+  return (
+    <div className="App">
+          <Hero/>
+          <Programs/>
+          <Reasons/>
+          <Plans/>
+          <Testimonials/>
+          <Join/>
+          <Footer/>
+    </div>
+  );
+}
+
+export default App;
